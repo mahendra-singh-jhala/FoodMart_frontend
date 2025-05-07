76f4672f38f81d1e5bdab08526d892725cf9e08b
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-07 20:57:56 +0530

    first commit

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,38 +1,90 @@
 import React from 'react'
 import loginImg from '../../asset/banner-image-2.jpg'
 import { Link } from 'react-router-dom'
+import { useDispatch } from "redux"
+import { register } from '../../state/auth/Action'
 
 const Register = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target)
+        const userData = {
+            usename: data.get("username"),
+            firstname: data.get("firstname"),
+            lastname: data.get("lastname"),
+            email: data.get("email"),
+            password: data.get("password")
+        }
+        dispatch(register(userData))
+    }
+
     return (
         <div className="bg-warning bg-gradient min-vh-100 d-flex justify-content-center align-items-center">
             <div className="bg-white d-flex justify-content-between w-75 h-50 shadow rounded-3">
                 <div className="w-100 d-none d-lg-block">
                     <img src={loginImg} alt="loginImg" className="img-fluid w-100 h-100 rounded-start-3" />
                 </div>
-                <div className="rounded-3 w-100 p-4">
+                <div className="rounded-3 w-100 p-4" onSubmit={handleSubmit}>
                     <h1 className="text-center"> Sign Up </h1>
-                    <form action="" className="p-4">
+                    <form className="p-4">
                         <div className="mb-3">
-                            <label for="text" className="form-label fw-semibold">Username</label>
-                            <input type="text" className="form-control form-control-lg fs-6" id="text" placeholder="Username" />
+                            <label for="username" className="form-label fw-semibold">Username</label>
+                            <input 
+                                type="text" 
+                                id="username" 
+                                name="username" 
+                                placeholder="Username..." 
+                                className="form-control form-control-lg fs-6" 
+                                required
+                            />
                         </div>
                         <div className="row mb-3">
                             <div className="col-6">
                                 <label for="firstname" className="form-label fw-semibold">First Name</label>
-                                <input type="text" className="form-control form-control-lg fs-6" id="firstname" placeholder="first name" />
+                                <input 
+                                    type="text" 
+                                    id="firstname" 
+                                    name="firstname"
+                                    placeholder="first name"
+                                    className="form-control form-control-lg fs-6"
+                                    required
+                                />
                             </div>
                             <div className="col-6">
                                 <label for="last name" className="form-label fw-semibold">Last Name</label>
-                                <input type="text" className="form-control form-control-lg fs-6" id="last name" placeholder="last name" />
+                                <input 
+                                    type="text" 
+                                    id="lastname" 
+                                    name="lastname"
+                                    placeholder="last name" 
+                                    className="form-control form-control-lg fs-6" 
+                                    required
+                                />
                             </div>
                         </div>
                         <div className="mb-3">
                             <label for="email" className="form-label fw-semibold">Email</label>
-                            <input type="emai" className="form-control form-control-lg fs-6" id="email" placeholder="Your Email" />
+                            <input 
+                                type="emai" 
+                                id="email"
+                                name="email" 
+                                placeholder="Your Email" 
+                                className="form-control form-control-lg fs-6"
+                                required
+                            />
                         </div>
                         <div className="mb-3">
                             <label for="password" className="form-label fw-semibold">Password</label>
-                            <input type="password" className="form-control form-control-lg fs-6" id="password" placeholder="Password" />
+                            <input 
+                                type="password"  
+                                id="password" 
+                                name="password"
+                                placeholder="Password" 
+                                className="form-control form-control-lg fs-6"
+                                required
+                            />
                         </div>
                         <Link to="/login" className="text-primary link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Already have an account?</Link>
                         <div className="mb-3 mt-1">
