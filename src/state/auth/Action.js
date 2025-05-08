81e3815b9013f1b267aeaf154fcd47cdd3d028a6
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-08 20:47:37 +0530

    first commit

--- src/state/auth/Action.js
+++ src/state/auth/Action.js
@@ -1,5 +1,5 @@
 import { api } from "../../config/API"
-import { REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType"
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType"
 
 
 export const register = (userData) => async (dispatch) => {
@@ -7,9 +7,22 @@ export const register = (userData) => async (dispatch) => {
     try {
         const res = await api.post("/api/auth/register", userData)
         const data = res.data
-
         dispatch({ type: REGISTER_SUCCESS, payload: data})
     } catch (error) {
         dispatch({ type: REGISTER_FAILURE, payload: error })
+    }
+}
+
+export const login = (useData) => async (dispatch) => {
+    dispatch({ type: LOGIN_REQUEST })
+    try {
+        const res = await api.post("/api/auth/login", useData)
+        const user = res.data
+        if(user) {
+            localStorage.setItem("token", JSON.stringify(user))
+        }
+        dispatch({ type: LOGIN_SUCCESS, payload: user })
+    } catch (error) {
+        dispatch({ type: LOGIN_FAILURE, payload: error })
     }
 }
