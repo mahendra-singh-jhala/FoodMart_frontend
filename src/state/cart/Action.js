3c88586eb5a35dc0d54aae15ee34f1f2ec68070d
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-23 16:30:49 +0530

    implement cart functionality

---
+++ src/state/cart/Action.js
@@ -0,0 +1,38 @@
+import { api } from '../../config/API'
+import { ADD_ITEM_TO_CART_FAILURE, ADD_ITEM_TO_CART_REQUEST, ADD_ITEM_TO_CART_SUCCESS, GET_CART_FAILURE, GET_CART_REQUEST, GET_CART_SUCCESS } from './ActionType'
+
+// fetch the user cart
+export const getCart = () => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: GET_CART_REQUEST })
+    try {
+        const res = await api.get("/api/cart", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = res.data
+        dispatch({ type: GET_CART_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({ type: GET_CART_FAILURE, payload: error.message })
+    }
+}
+
+// add item to the cart
+export const addItemToCart = () => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: ADD_ITEM_TO_CART_REQUEST })
+    try {
+        const res = await api.get("/api/cart/add", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = res.data
+        dispatch({ type: ADD_ITEM_TO_CART_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({ type: ADD_ITEM_TO_CART_FAILURE, payload: error.message })
+    }
+}
