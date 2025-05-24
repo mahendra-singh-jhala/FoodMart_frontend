95cf751265527f5fdd8a43579d24777979fc1168
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-24 17:12:51 +0530

    Implement the update & delete cart reducer

--- src/state/cart/Reducer.js
+++ src/state/cart/Reducer.js
@@ -1,5 +1,5 @@
 import CartItems from "../../../../server/models/cartItem"
-import { ADD_ITEM_TO_CART_FAILURE, ADD_ITEM_TO_CART_REQUEST, ADD_ITEM_TO_CART_SUCCESS, GET_CART_FAILURE, GET_CART_REQUEST, GET_CART_SUCCESS } from "./ActionType"
+import { ADD_ITEM_TO_CART_FAILURE, ADD_ITEM_TO_CART_REQUEST, ADD_ITEM_TO_CART_SUCCESS, DELETE_CART_ITEM_FAILURE, DELETE_CART_ITEM_REQUEST, DELETE_CART_ITEM_SUCCESS, GET_CART_FAILURE, GET_CART_REQUEST, GET_CART_SUCCESS, UPDATE_CART_ITEM_FAILURE, UPDATE_CART_ITEM_REQUEST, UPDATE_CART_ITEM_SUCCESS } from "./ActionType"
 
 const initialState = {
     cart: null,
@@ -16,11 +16,27 @@ export const cartReducer = (state = initialState, action) => {
             return { ...state, isLoading: false, cartItem: action.payload.cartItem, cart: action.payload.cart,  error: null }
         case GET_CART_FAILURE:
             return { ...state, isLoading: false, error: action.payload }
+
         case ADD_ITEM_TO_CART_REQUEST:
             return { ...state, isLoading: true, error: null }
         case ADD_ITEM_TO_CART_SUCCESS:
             return { ...state, isLoading: false, cartItem: [...state.cartItem, action.payload.cartItem] }
         case ADD_ITEM_TO_CART_FAILURE:
             return { ...state, isLoading: false, error: action.payload }
+
+        case UPDATE_CART_ITEM_REQUEST:
+        case DELETE_CART_ITEM_REQUEST:
+            return { ...state, isLoading: true, error: null }
+
+        case UPDATE_CART_ITEM_SUCCESS:
+            return { ...state, isLoading: false, cartItem: action.payload, error: null}
+        
+        case DELETE_CART_ITEM_SUCCESS:
+            return { ...state, isLoading: false, cartItem: action.payload, error: null }
+
+        case UPDATE_CART_ITEM_FAILURE:
+        case DELETE_CART_ITEM_FAILURE:
+            return { ...state, isLoading: false, error: action.payload }
+
     }
 }
