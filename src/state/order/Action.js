3edda831c9ef8f82d0a5c3d489b4272df110b2c7
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-26 16:35:40 +0530

    implement get order action

--- src/state/order/Action.js
+++ src/state/order/Action.js
@@ -1,5 +1,5 @@
 import { api } from "../../config/API"
-import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS } from "./ActionType"
+import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, GET_ORDER_BY_ID_FAILURE, GET_ORDER_BY_ID_REQUEST, GET_ORDER_BY_ID_SUCCESS } from "./ActionType"
 
 // create an order
 export const createOrder = (reqData) => async (dispatch) => {
@@ -17,3 +17,21 @@ export const createOrder = (reqData) => async (dispatch) => {
         dispatch({ type: CREATE_ORDER_FAILURE, payload: error.message })
     }
 }
+
+// fetch a specific order by ID
+export const getOrderById = (orderId) => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: GET_ORDER_BY_ID_REQUEST })
+    try {
+        const res = await api.post(`/api/order/${orderId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = res.data
+        dispatch({ type: GET_ORDER_BY_ID_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({ type: GET_ORDER_BY_ID_FAILURE, payload: error.message })
+    }
+}
+
