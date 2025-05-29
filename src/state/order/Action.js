247e4e550fe5b3d888b3fc14d25e8e9b71f05312
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-29 16:17:23 +0530

    implement get all user order

--- src/state/order/Action.js
+++ src/state/order/Action.js
@@ -1,5 +1,5 @@
 import { api } from "../../config/API"
-import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, GET_ORDER_BY_ID_FAILURE, GET_ORDER_BY_ID_REQUEST, GET_ORDER_BY_ID_SUCCESS } from "./ActionType"
+import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, GET_ORDER_BY_ID_FAILURE, GET_ORDER_BY_ID_REQUEST, GET_ORDER_BY_ID_SUCCESS, GET_ORDER_USER_FAILURE, GET_ORDER_USER_REQUEST, GET_ORDER_USER_SUCCESS } from "./ActionType"
 
 // create an order
 export const createOrder = (reqData) => async (dispatch) => {
@@ -35,3 +35,19 @@ export const getOrderById = (orderId) => async (dispatch) => {
     }
 }
 
+// fetch a all order
+export const getOrders = () => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: GET_ORDER_USER_REQUEST })
+    try {
+        const res = await api.post(`/api/order`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = res.data
+        dispatch({ type: GET_ORDER_USER_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({ type: GET_ORDER_USER_FAILURE, payload: error.message })
+    }
+}
