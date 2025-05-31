cf14930a5b2b85b7244e45235b0ad21465e97d2f
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-05-31 16:02:10 +0530

    implement use update profile logic

---
+++ src/state/users/user/Action.js
@@ -0,0 +1,19 @@
+import { api } from "../../../config/API"
+import { UPDATE_USER_PROFILE_FAILURE, UPDATE_USER_PROFILE_REQUEST, UPDATE_USER_PROFILE_SUCCESS } from "./ActionType"
+
+
+export const updatProfile = (userData) => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: UPDATE_USER_PROFILE_REQUEST })
+    try {
+        const res = await api.put("/api/users/profileUpdate", userData, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = res.data
+        dispatch({ type: UPDATE_USER_PROFILE_SUCCESS, payload: data})
+    } catch (error) {
+        dispatch({ type: UPDATE_USER_PROFILE_FAILURE, payload: error.message })
+    }
+} 
