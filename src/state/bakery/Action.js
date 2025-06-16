a64e7b33db8d06bbe85460cee13cccd1f04d3d8b
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-06-16 13:08:51 +0530

    implement create product functionallity

--- src/state/bakery/Action.js
+++ src/state/bakery/Action.js
@@ -1,10 +1,10 @@
 import { CREATE_BAKERY_FAILURE, CREATE_BAKERY_REQUEST, CREATE_BAKERY_SUCCESS, GET_BAKERY_FAILURE, GET_BAKERY_REQUEST, GET_BAKERY_SUCCESS } from "./ActionType"
 
-export const createBakery = () => async (dispatch) => {
+export const createBakery = (bakeryData) => async (dispatch) => {
     const token = JSON.parse(localStorage.getItem("token")).token
     dispatch({ type: CREATE_BAKERY_REQUEST })
     try {
-        const res = await api.post("/api/bakery", {
+        const res = await api.post("/api/bakery", { bakeryData }, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
@@ -26,7 +26,7 @@ export const getBakery = () => async (dispatch) => {
                 Authorization: `Bearer ${token}`
             }
         })
-
+        
         const bakery = res.data
         dispatch({ type: GET_BAKERY_SUCCESS, payload: bakery })
     } catch (error) {
--- src/state/product/Action.js
+++ src/state/product/Action.js
@@ -1,7 +1,26 @@
-import { FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS } from "./ActionType"
+import { CREATE_PRODUCT_FAILURE, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS } from "./ActionType"
 import { api } from '../../config/API'
 
-// find products
+// create product
+export const createFoodProduct = (FoodData) => async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem("token")).token
+    dispatch({ type: CREATE_PRODUCT_REQUEST })
+    try {
+        const res = await api.post('/api/product', { FoodData }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = res.data
+        dispatch({ type: CREATE_PRODUCT_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({ type: CREATE_PRODUCT_FAILURE, payload: error.message })
+    }
+}
+
+
+// find all products
 export const findFoodProduct = () => async (dispatch) => {
     dispatch({ type: FIND_PRODUCTS_REQUEST })
     try {
